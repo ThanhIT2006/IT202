0ee0c950795142ab012f02f21e18906eff705845
ThanhIT2006 <nguyenngocthanh15122006@gmail.com>
2026-01-10 22:21:26 +0700

    BT

--- SS10/BT5.sql
+++ SS10/BT5.sql
@@ -1,4 +1,3 @@
--- 4) kiểm tra kế hoạch thực thi trước khi có chỉ mục
 explain analyze select u.user_id, u.username, p.post_id, p.content from users u
 join posts p on p.user_id = u.user_id
 where u.hometown = 'Hà Nội'
